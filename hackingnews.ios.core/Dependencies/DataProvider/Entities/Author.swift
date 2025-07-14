7b91f4aaa0efc8e6bf57e407654c2273412ebf15
Tonni Hyldgaard <tohy@dsb.dk>
2025-07-14 23:07:14 +0200

    Added Story and Author entity types

---
+++ hackingnews.ios.core/Dependencies/DataProvider/Entities/Author.swift
@@ -0,0 +1,16 @@
+//
+//  Author.swift
+//  hackingnews.ios.core
+//
+//  Created by Tonni Hyldgaard on 14/07/2025.
+//
+
+import Foundation
+
+struct Author: Codable, Hashable {
+    let id: String
+    let about: String
+    let created: Int
+    let karma: Int
+    let submitted: [Int]
+}
---
+++ hackingnews.ios.core/Dependencies/DataProvider/Entities/Story.swift
@@ -0,0 +1,19 @@
+//
+//  Story.swift
+//  hackingnews.ios.core
+//
+//  Created by Tonni Hyldgaard on 14/07/2025.
+//
+
+import Foundation
+
+struct Story: Codable, Hashable {
+    let id: Int
+    let title: String
+    let by: String
+    let url: String
+    let score: Int
+    let time: Int
+    let type: String
+    let descendants: Int
+}
