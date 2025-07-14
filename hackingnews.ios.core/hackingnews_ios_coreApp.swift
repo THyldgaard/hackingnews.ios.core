f64ac9251d6b6bc073c9c5a3b8785c33b166bd05
Tonni Hyldgaard <tohy@dsb.dk>
2025-07-14 22:22:34 +0200

    Initial Commit

---
+++ hackingnews.ios.core/hackingnews_ios_coreApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackingnews_ios_coreApp.swift
+//  hackingnews.ios.core
+//
+//  Created by Tonni Hyldgaard on 14/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct hackingnews_ios_coreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hackingnews.ios.coreTests/hackingnews_ios_coreTests.swift
@@ -0,0 +1,17 @@
+//
+//  hackingnews_ios_coreTests.swift
+//  hackingnews.ios.coreTests
+//
+//  Created by Tonni Hyldgaard on 14/07/2025.
+//
+
+import Testing
+@testable import hackingnews_ios_core
+
+struct hackingnews_ios_coreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
